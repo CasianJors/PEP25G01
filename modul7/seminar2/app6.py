6d642fddb832283948316e7d20601c732fcdab9c
Casian <57826342+CasianJors@users.noreply.github.com>
2025-03-25 18:19:16 +0200

    m8

--- modul7/seminar2/app6.py
+++ modul7/seminar2/app6.py
@@ -1,29 +1,28 @@
 class Player:
     def __init__(self, name):
-        # TODO: Inițializează atributele private _name și _health (100)
-        pass
+        self.name = name
+        self.health = 100
 
     def attack(self, boss):
-        # TODO: Alegeți o valoare de damage (ex: 50) și reduceți sănătatea bossului folosind metoda boss.take_damage(damage)
-        pass
+        boss.take_damage(500)
 
     def get_health(self):
-        # TODO: Returnează valoarea actuală a _health
-        pass
+        return self.health
 
 
 class Boss:
     def __init__(self, name):
-        # TODO: Inițializează atributele private _name și _health (500)
-        pass
+        self.name = name
+        self.health = 500
 
     def take_damage(self, amount):
-        # TODO: Scade sănătatea bossului cu amount, asigurându-te că _health nu scade sub 0
-        pass
+        self.health -= amount
 
     def is_defeated(self):
-        # TODO: Returnează True dacă _health este 0, altfel False
-        pass
+        if self.health <= 0:
+            return True
+        else:
+            return False
 
 
 # Exemplu de utilizare:
@@ -33,17 +32,10 @@ if __name__ == "__main__":
 
     # Simularea unui atac
     player.attack(boss)
-    print("Sănătatea bossului:", boss._health)  # Așteptat: 500 - damage_value
+    print("Sănătatea bossului:", boss.health)  # Așteptat: 500 - damage_value
 
     # Verificați dacă bossul este învins după un atac
     if boss.is_defeated():
         print("Boss învins!")
     else:
         print("Bossul încă luptă!")
-
-
-"""             HOMEWORK            """
-
-# TODO   -Boss also takes player's damage each time he is attacked ( example: player damages boss -> boss takes damage -> boss damages back)
-#        -Boss' damage is a random number between 1 and 10 (import random)
-#        -Make the game run in a loop, until one of the players is defeated (for this, you will also need to implement player take damage and is defeated)
